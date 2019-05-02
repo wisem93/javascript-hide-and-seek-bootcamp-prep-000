8a37080ea9a7578e91a9374a5e15859674475381
Marcus Wiseman <marcus.e.wiseman@gmail.com>
2019-05-02 03:21:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector(".target");
+}
+
+function increaseRankBy(n) {
+  var lis = document.getElementById("app").querySelectorAll("ul.ranked-list li");
+  
+  for(let i=0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  var children = document.getElementById("grand-node").querySelectorAll("div");
+  return children[children.length-1];
+}
 
