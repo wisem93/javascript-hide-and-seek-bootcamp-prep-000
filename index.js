5a92110b196efa3d17328521df3f75bca8b98746
Marcus Wiseman <marcus.e.wiseman@gmail.com>
2019-05-02 02:30:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector("#nested div div div div.target");
+}
+
+function increaseRankBy(n) {
+  
+}
